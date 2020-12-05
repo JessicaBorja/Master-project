7c36f1309f4c4122b25d009da65e6f70f5b2147c
Jessica Borja <jessicalizethborja@hotmail.com>
2020-12-05 11:44:28 +0100

    train gym environments w/hydra

--- train.py
+++ train.py
@@ -5,42 +5,19 @@ from gym.wrappers.monitoring.video_recorder import VideoRecorder
 # from pybullet_envs.gym_manipulator_envs import ReacherBulletEnv, PusherBulletEnv
 # from pybullet_envs.gym_locomotion_envs import HalfCheetahBulletEnv
 # from pybullet_envs.bullet.kukaGymEnv import KukaGymEnv
-from gym.envs.mujoco import mujoco_env
-import mujoco_py
-from utils.utils import read_optim_results
+# from gym.envs.mujoco import mujoco_env
+# import mujoco_py
 from sac import SAC
-from datetime import datetime
-import time, os, pickle
+import hydra
 
-hyperparameters = {
-    "gamma": 0.99,
-    "actor_lr": 1e-5, 
-    "critic_lr": 0.00035, 
-    "alpha_lr": 1e-5,
-    "alpha": "auto",#"auto",
-    "tau": 0.008,
-    "batch_size": 220,
-    "buffer_size": 1e6,
-    "train_freq": 1, #timesteps collectng data
-    "gradient_steps": 1, #timesteps updating gradients
-    "learning_starts": 1000, #timesteps before starting updates
-    "hidden_dim": 470
-}
-
-learn_configuration = {
-    "total_timesteps": int(1e6), #millon 1e6
-    "log_interval": 1000, #log timestep reward every log_interval steps
-    "max_episode_length": 200, #max episode length
-}
-
-def train(env_name):
+@hydra.main(config_path="./config", config_name="config_gym")
+def train(cfg):
+    env_name = "Pendulum-v0"
+    hyperparameters = cfg.agent.hyperparameter
     hyperparameters["env"] = gym.make(env_name).env
     hyperparameters["eval_env"] = gym.make(env_name).env
-    hyperparameters["model_name"] = "sac_mujocoPusher"
     model = SAC(**hyperparameters)
-    model.learn(**learn_configuration)
+    model.learn(**cfg.agent.learn_config)
     
 if __name__ == "__main__":
-    env_name = "Pusher-v2"
-    train(env_name)
-    #read_optim_results("sac_mujocoPusher2.pkl")
+    train()

a2642bb4785d8418d445279f7a08bd5fdd819ac3
JessicaBorja <jessicalizethborja@gmail.com>
2021-08-07 15:12:02 +0200

    scale of env target

--- combined/target_search.py
+++ combined/target_search.py
@@ -54,7 +54,7 @@ class TargetSearch():
         target_pos, _ = env.get_target_pos()
         # 2 cm deviation
         target_pos = np.array(target_pos)
-        target_pos += np.random.normal(loc=0, scale=0.01,
+        target_pos += np.random.normal(loc=0, scale=[0.01, 0.01, 0.02],
                                        size=(len(target_pos)))
 
         # always returns a target position
